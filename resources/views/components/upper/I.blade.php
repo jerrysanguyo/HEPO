2b1cd9851081dce84fa1e27faa545d0cf55df43e
jerrysanguyo <jsanguyo1624@gmail.com>
2024-11-24 11:50:19 +0800

    done with IU

--- resources/views/components/upper/I.blade.php
+++ resources/views/components/upper/I.blade.php
@@ -9,20 +9,96 @@
     </div>
     <HR></HR>
     <div class="row mt-3">
-        <div class="col-lg-3 col-md-3 col-sm-12">
-
+        <div class="col-lg-3 col-md-12 col-sm-12">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <img src="{{ asset('images/se1.webp') }}" alt="" class="image-fluid">
+                </div>
+                <div class="mt-3 col-lg-12 col-md-12 col-sm-12">
+                    <img src="{{ asset('images/se2.webp') }}" alt="" class="image-fluid">
+                </div>
+            </div>
         </div>
-        <div class="col-lg-3 col-md-3 col-sm-12">
-
+        <div class="col-lg-3 col-md-12 col-sm-12">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <img src="{{ asset('images/se3.webp') }}" alt="" class="image-fluid">
+            </div>
         </div>
-        <div class="col-lg-9 col-md-9 col-sm-12">
-            <p class="fs-4">
+        <div class="col-lg-6 col-md-9 col-sm-12">
+            <p class="fs-5">
                 The Taguig City Disability Resource and Development Center in Barangay North Signal was launched on January 9, 2024, providing 
                 a space where PWDs can access free consultations, exchange information, and further empower themselves. The center, spread across six floors, 
                 is equipped with complete facilities for those in need of free services such as therapy and consultation. These include free Speech Therapy, 
                 Adult and Pediatric Physical Therapy, Occupational Therapy, along with consultations from Rehabilitation Medicine doctors, Developmental Pediatricians, 
                 Physical Therapists, Occupational Therapists, and Speech Therapists. The center will also process applications and issue PWD IDs, 
                 as well as oversee livelihood and training programs.
+            </p>
+        </div>
+    </div>
+    <hr>
+    <div class="row mt-3">
+        <div class="col-lg-6 col-md-9 col-sm-12">
+            <p class="fs-5">
+                A new multipurpose building in Brgy. Calzada-Tipas designed to provide critical services to the community while serving as a venue for community 
+                events was launched on February 2, 2024. The building houses a health center at the ground floor which offers a wide range of services that 
+                cater to the community’s needs, including TB DOTS room, minor surgical room, examination room, physician’s office, prenatal/family planning room, 
+                dental clinic, treatment room, and breastfeeding room.
+            </p>
+        </div>
+        <div class="col-lg-3 col-md-12 col-sm-12">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <img src="{{ asset('images/se4.webp') }}" alt="" class="image-fluid">
+                </div>
+                <div class="mt-3 col-lg-12 col-md-12 col-sm-12">
+                    <img src="{{ asset('images/se5.webp') }}" alt="" class="image-fluid">
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-12 col-sm-12">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <img src="{{ asset('images/se6.webp') }}" alt="" class="image-fluid">
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="row mt-3">
+        <div class="col-lg-3 col-md-12 col-sm-12">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <img src="{{ asset('images/se1.webp') }}" alt="" class="image-fluid">
+                </div>
+                <div class="mt-3 col-lg-12 col-md-12 col-sm-12">
+                    <img src="{{ asset('images/se2.webp') }}" alt="" class="image-fluid">
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-12 col-sm-12">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <img src="{{ asset('images/se3.webp') }}" alt="" class="image-fluid">
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-9 col-sm-12">
+            <p class="fs-5">
+                The City of Taguig, through its City Nutrition Office, held the Healthy Street Food Festival on Friday, July 12, at the Employee’s Park, Taguig City Hall.
+                The event saw enthusiastic participation from city hall department heads and employees, who sampled a variety of healthy and nutritious 
+                street food and drinks prepared by the City Nutrition Office.
+            </p>
+            <p class="mt-3 fs-5">
+            Food stalls showcased a range of healthy options, including:
+                <ul class="list-group fs-6">
+                    <li class="list-group-item">Kangkong Chips</li>
+                    <li class="list-group-item">Carrot Cheesesticks</li>
+                    <li class="list-group-item">Banana Blossom Balls</li>
+                    <li class="list-group-item">Chicken Veggie Balls</li>
+                    <li class="list-group-item">Veggie Burgers</li>
+                    <li class="list-group-item">Fruits on the Go</li>
+                    <li class="list-group-item">Kamote Balls (Buchi-buchi)</li>
+                    <li class="list-group-item">Healthy Refreshing Juices (Tanglad, Cucumber, Sampaloc, and Kamote Tops)</li>
+                </ul>
+            </p>
+            <p class="fs-5 mt-3">
+                Participants were given rating cards to evaluate the food and drinks based on appearance, flavor, aroma, texture, affordability, and general acceptability.
             </p>
         </div>
     </div>
--- resources/views/components/upper/U.blade.php
+++ resources/views/components/upper/U.blade.php
@@ -1,3 +1,46 @@
-<div>
-    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
+<div class="container">
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 text-center">
+            <img src="{{ asset('images/Picture1.webp') }}" alt="" class="img-thumbnail border-0">
+        </div>
+    </div>
+    <div class="row">
+        <p class="fs-1 text-center">Major accomplishment (Supportive Environment)</p>
+    </div>
+    <HR></HR>
+    <div class="row mt-3">
+        <div class="col-lg-3 col-md-12 col-sm-12 ">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12">
+                    <img src="{{ asset('images/hpp1.webp') }}" alt="" class="image-fluid">
+                </div>
+                <div class="mt-3 col-lg-12 col-md-12 col-sm-12">
+                    <img src="{{ asset('images/hpp2.webp') }}" alt="" class="image-fluid">
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-12 col-sm-12">
+            <div class="col-lg-12 col-md-12 col-sm-12 text-center">
+                <img src="{{ asset('images/hpp3.webp') }}" alt="" class="image-fluid">
+            </div>
+        </div>
+        <div class="col-lg-6 col-md-9 col-sm-12">
+            <p class="">
+                <ul class="list-group fs-5">
+                    <li class="list-group-item">
+                        A Resolution Authorizing the Creation of the Health Promotion Unit (HPU) Under the Taguig City Health Office and Endorsing 
+                        the Implementation of Health Promotion Policies, Programs, and Activities in Alignment with Republic Act No. 
+                        11223 (Universal Health Care Act).
+                    </li>
+                    <li class="list-group-item">
+                        An Order Creating the Health Promotion Unit of the Taguig City Health Office for the Governance and Implementation of 
+                        Health Promotion Policies, Programs, and Activities in the City-Wide Health System of the City of Taguig.
+                    </li>
+                    <li class="list-group-item">
+                        A Resolution Adopting the Health Promotion Framework Strategy 2030 for the City-Wide Health System of the City of Taguig.
+                    </li>
+                </ul>
+            </p>
+        </div>
+    </div>
 </div>
